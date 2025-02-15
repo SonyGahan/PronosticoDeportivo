0233e6e1e42cc2a9c813345d15ec288becf6bfb4
SonyGahan <sonygahan@gmail.com>
2025-02-15 19:50:40 -0300

    Diseño y estructura de Pronosticos Deportivos - Git Ignore

--- BD codigo.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE pronosticos_deportivos;
-
-CREATE USER 'pronosticos_user'@'localhost' IDENTIFIED BY 'User1234';
-GRANT ALL PRIVILEGES ON pronosticos_deportivos.* TO 'pronosticos_user'@'localhost';
-FLUSH PRIVILEGES;
-
-SELECT User, Host FROM mysql.user;
